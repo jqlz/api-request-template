8557b9b2ec1240ca0e7d72650a225a71c72458b5
jqlz <105872600+jqlz@users.noreply.github.com>
2025-01-11 14:54:14 +0000

    python

---
+++ py.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://api.example.com/data'
+headers = {
+    'Authorization': 'Bearer YOUR_TOKEN',
+    'Content-Type': 'application/json'
+}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+print(data)
